f674c91e3e8c955f3528e1b15febacc7a652013c
lewbon61 <88298597+lewbon61@users.noreply.github.com>
2023-06-16 12:25:49 +1200

    finished enemy basic code

--- 2D game/Assets/Scripts/Enemy.cs
+++ 2D game/Assets/Scripts/Enemy.cs
@@ -4,45 +4,70 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+      [SerializeField]
   private float enemyHealth;
 
+      [SerializeField]
   private float movementSpeed;
 
   private int killReward;
   private int damage;
 
+  private GameObject targetTile;
+
+
   private void Start()
   {
-
+      initializeEnemy();
   }
 
   private void initializeEnemy()
   {
-        targetTile = MapGenerator.startTile;
+      targetTile = MapGenerator.startTile;
   }
+
+
+      public void takeDamage(float amount)
+      {
+            enemyHealth -= amount;
+
+            if (enemyHealth <= 0)
+            {
+                  die();
+            }
+      }
+
+      private void die()
+      {
+            Destroy(transform.gameObject);
+      }
 
   private void moveEnemy()
   {
-        transform.position = Vector3.MoveTowards(transform.position, targetTile.transform, movementSpeed * Time.deltatime);
-
+        transform.position = Vector3.MoveTowards(transform.position, targetTile.transform.position, movementSpeed * Time.deltaTime);
   }
 
   private void checkPosition()
   {
         if (targetTile != null && targetTile != MapGenerator.endTile)
         {
-            float distance = (transform.position - targetTile.trandform.positon).magnitude;
+            float distance = (transform.position - targetTile.transform.position).magnitude;
 
             if (distance < 0.001f)
             {
                 int currentIndex = MapGenerator.pathTiles.IndexOf(targetTile);
+
+                targetTile = MapGenerator.pathTiles[currentIndex + 1];
             }
         }
   }
 
   private void Update()
   {
+      checkPosition();
+      moveEnemy();
 
+      takeDamage(0);
   }
 
 }
