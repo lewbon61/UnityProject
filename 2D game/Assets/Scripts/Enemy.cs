281b9713f69a80f93b1109d156b7831e5a726745
lewbon61 <88298597+lewbon61@users.noreply.github.com>
2023-06-20 14:57:38 +1200

    Starting Tower!! Problem with code inheritence!!
    continue the video from 13:32

--- 2D game/Assets/Scripts/Enemy.cs
+++ 2D game/Assets/Scripts/Enemy.cs
@@ -4,70 +4,75 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-      [SerializeField]
-  private float enemyHealth;
+    [SerializeField]
+    private float enemyHealth;
 
-      [SerializeField]
-  private float movementSpeed;
+    [SerializeField]
+    private float movementSpeed;
 
-  private int killReward;
-  private int damage;
+    private int killReward;
+    private int damage;
 
-  private GameObject targetTile;
+    private GameObject targetTile;
+
+    private void Awake()
+    {
+        Enemies.enemies.Add(gameObject);
+    }
+
+    private void Start()
+    {
+        initializeEnemy();
+    }
+
+    private void initializeEnemy()
+    {
+        targetTile = MapGenerator.startTile;
+    }
 
 
-  private void Start()
-  {
-      initializeEnemy();
-  }
+    public void takeDamage(float amount)
+    {
+        enemyHealth -= amount;
 
-  private void initializeEnemy()
-  {
-      targetTile = MapGenerator.startTile;
-  }
-
-
-      public void takeDamage(float amount)
-      {
-            enemyHealth -= amount;
-
-            if (enemyHealth <= 0)
-            {
-                  die();
-            }
-      }
-
-      private void die()
-      {
-            Destroy(transform.gameObject);
-      }
-
-  private void moveEnemy()
-  {
-        transform.position = Vector3.MoveTowards(transform.position, targetTile.transform.position, movementSpeed * Time.deltaTime);
-  }
-
-  private void checkPosition()
-  {
-        if (targetTile != null && targetTile != MapGenerator.endTile)
+        if (enemyHealth <= 0)
         {
-            float distance = (transform.position - targetTile.transform.position).magnitude;
-
-            if (distance < 0.001f)
-            {
-                int currentIndex = MapGenerator.pathTiles.IndexOf(targetTile);
-
-                targetTile = MapGenerator.pathTiles[currentIndex + 1];
-            }
+            die();
         }
-  }
+    }
 
-  private void Update()
-  {
-      checkPosition();
-      moveEnemy();
+    private void die()
+    {  
+        Enemies.enemies.Remove(gameObject);
+        Destroy(transform.gameObject);
+    }
 
-      takeDamage(0);
-  }
+    private void moveEnemy()
+    {
+          transform.position = Vector3.MoveTowards(transform.position, targetTile.transform.position, movementSpeed * Time.deltaTime);
+    }
+
+    private void checkPosition()
+    {
+            if (targetTile != null && targetTile != MapGenerator.endTile)
+            {
+                float distance = (transform.position - targetTile.transform.position).magnitude;
+
+                if (distance < 0.001f)
+                {
+                    int currentIndex = MapGenerator.pathTiles.IndexOf(targetTile);
+
+                    targetTile = MapGenerator.pathTiles[currentIndex + 1];
+                }
+            }
+    }
+
+    private void Update()
+    {
+        checkPosition();
+        moveEnemy();
+
+        takeDamage(0);
+    }
 
 }
---
+++ 2D game/Assets/Scripts/Tower.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float range;
+    [SerializeField] private float damage;
+    [SerializeField] private float timeBetweenShots; //in seconds
+
+    private float nextTimeToShoot;
+
+    private GameObject currentTarget;
+
+    private void start()
+    {
+        nextTimeToShoot = Time.time;
+    }
+
+    private void updateNearestEnemy()
+    {
+        GameObject currentNearestEnemy = null;
+
+        float distance = Mathf.Infinity;
+
+        foreach(GameObject enemy in Enemies.enemies)
+        {
+            float _distance = (transform.position - enemy.transform.position).magnitude;
+
+            if (_distance < distance)
+            {
+                distance = _distance;
+                currentNearestEnemy = enemy;
+            }
+        }
+
+        if (distance <= range)
+        {
+            currentTarget = currentNearestEnemy;
+        }
+        else
+        {
+            currentTarget = null;
+        }
+    }
+
+    private void shoot()
+    {
+        Enemy enemyScript = currentTarget.GetComponent<Enemy>();
+
+        enemyScript.takeDamage(damage);
+    }
+
+    private void Update()
+    {
+        updateNearestEnemy();
+
+        if (Time.time >= nextTimeToShoot)
+        {
+            if (currentTarget != null)
+            {
+                shoot();
+                nextTimeToShoot = Time.time + timeBetweenShots;
+            }
+        }
+    }
+
+}
